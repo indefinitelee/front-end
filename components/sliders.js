321a2c615dc78dc3acd5a3e781760341241b4e4d
lee <leerwilson@gmail.com>
2017-03-24 14:40:18 -0400

    trying to slide

---
+++ components/sliders.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var React = require('react');
+var ReactNative= require('react-native');
+var {
+  Slider,
+  Text,
+  StyleSheet,
+  View,
+} = ReactNative;
+
+export default class App extends React.Component {
+  static defaultProps = {
+    value: 0,
+    minimumValue: 0,
+    maximumValue: 100,
+    step:1,
+  };
+
+  state = {
+    value: this.props.value,
+    minimumValue: this.props.minimumValue,
+    maximumValue: this.props.maximumValue,
+    step: this.props.step,
+  };
+
+  render() {
+    return (
+      <View>
+        <Slider
+          {...this.props}
+          onSlidingComplete={(value) => this.setState({
+            slideCompletionValue: value,
+            slideCompletionCount: this.state.slideCompletionCount + 1})} />
+        <Text>
+          Completions: {this.state.slideCompletionCount} Value: {this.state.slideCompletionValue}
+        </Text>
+      </View
+    );
+  }
+}
+
+
